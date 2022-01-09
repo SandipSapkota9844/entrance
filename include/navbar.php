5aa5a9217c8e8d2b7ecfb7b034dd4ba99f9b6afd
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-01-09 10:00:37 +0545

    registration

--- include/navbar.php
+++ include/navbar.php
@@ -1,7 +1,7 @@
 
 <div class="relative bg-white overflow-hidden">
   <div class="max-w-7xl mx-auto">
-    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+    <div class=" ">
       <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
         <polygon points="50,0 100,0 50,100 0,100" />
       </svg>
@@ -13,7 +13,7 @@
               <div class="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span class="sr-only">Workflow</span>
-                  <img class="h-8 w-auto sm:h-10" src="../img/logo.png">
+                  <img class="h-8 w-auto sm:h-10" src="img/logo.png">
                 </a>
                 <div class="-mr-2 flex items-center md:hidden">
                   <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
